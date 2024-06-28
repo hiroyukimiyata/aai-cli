dafbd8e99e71576c2bf6bd75a8361263cc439543
EijiIriyama <eiriyama@adobe.com>
2024-06-28 15:54:06 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <argument 1>"
+  exit 1
+fi
+
+# Assign the arguments to variables
+ARG1=$1
+
+# Execute the Node.js script with the arguments
+# Please write a cd command to move to the 
+node aai-cli.js "$ARG1"
